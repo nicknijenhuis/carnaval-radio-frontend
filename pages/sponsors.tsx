118b110e3dd38eccd2cece22c785e94e91822500
Nick Nijenhuis <nick@novana.nl>
2023-01-13 17:11:11 +0100

    Add logo's to sponsors page

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -1,38 +1,107 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import React from 'react'
-import { GET_ALL_SPONSORS } from '../graphql/sponsor_queries';
-import { Sponsor } from '../types/sponsorTypes';
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import Image from "next/image";
+import React from "react";
+import { GET_ALL_SPONSORS } from "../graphql/sponsor_queries";
+import {
+  Sponsor,
+  SponsorType,
+  GraphQLSponsor,
+  GraphQLSponsorType,
+} from "../types/sponsorTypes";
 
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_STRAPI_URL,
   cache: new InMemoryCache(),
 });
 
-
 interface Props {
+  sponsorTypes: SponsorType[];
   sponsors: Sponsor[];
 }
 
-const sponsors = ({sponsors}: Props) => {
-  return (
-    <ul>{sponsors.map(x => {return (
-      <li key={x.Name}>{x.Name}</li>
-    )})}</ul>
-  )
+const logoSizeMapping: {
+  [id: string]: string
+} = {
+  'S': 'w-16',
+  'M': 'w-24',
+  'L': 'w-32',
+  'XL': 'w-40',
+  'XXL': 'w-48',
 }
 
-export async function getStaticProps(){
-  const { data } = await client.query({query: GET_ALL_SPONSORS});
-
-  const sponsors: Sponsor[] = data.sponsors.data.map((x: { attributes: { Name: string; }; }) => { return {
-    Name: x.attributes.Name
-  } as Sponsor    
-  });
-
-  return { props: {
-      sponsors
-    }
-  }  
+const LogosPerType = ({ sponsors, logoSize }: { sponsors: Sponsor[], logoSize: string }) => {
+  const logoSizeClass = logoSizeMapping[logoSize]; 
+  
+  return (
+    <ul>
+      {sponsors.map((x) => {
+        return (
+          <li key={x.Name}>
+            <Image
+              className={logoSizeClass}
+              src={x.Logo.Url}
+              width={x.Logo.Width}
+              height={x.Logo.Height}
+              alt={`Logo van ${x.Name}`}
+            />
+            {x.Name}
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
-export default sponsors
+const Sponsors = ({ sponsorTypes, sponsors }: Props) => {
+  return (
+    <ul>
+      {sponsorTypes
+        .sort((st) => st.Order)
+        .map((st) => {
+          const sponsorsPerType = sponsors.filter((x) => x.TypeID === st.Id);
+          return (
+            <div key={st.Name}>
+              <h1>{st.Name}</h1>
+              <LogosPerType sponsors={sponsorsPerType} logoSize={st.LogoSize} />
+            </div>
+          );
+        })}
+    </ul>
+  );
+};
+
+export async function getStaticProps() {
+  const { data } = await client.query({ query: GET_ALL_SPONSORS });
+
+  const sponsors: Sponsor[] = data.sponsors.data.map((x: GraphQLSponsor) => {
+    return {
+      Name: x.attributes.Name,
+      Logo: {
+        Width: x.attributes.Logo.data.attributes.width,
+        Height: x.attributes.Logo.data.attributes.height,
+        Url: x.attributes.Logo.data.attributes.url,
+      },
+      TypeID: x.attributes.Type.data.id,
+    } as Sponsor;
+  });
+
+  const sponsorTypes: SponsorType[] = data.sponsorTypes.data.map(
+    (x: GraphQLSponsorType) => {
+      return {
+        Id: x.id,
+        Name: x.attributes.Name,
+        Order: x.attributes.Order,
+        LogoSize: x.attributes.LogoSize,
+      } as SponsorType;
+    }
+  );
+
+  return {
+    props: {
+      sponsorTypes,
+      sponsors,
+    },
+  };
+}
+
+export default Sponsors;
--- types/sponsorTypes.d.ts
+++ types/sponsorTypes.d.ts
@@ -1,18 +1,43 @@
+export type GraphQLSponsor = {
+  attributes: {
+    Name: string;
+    Logo: {
+      data: {
+        attributes: {
+          url: string;
+          width: number;
+          height: number;
+        };
+      };
+    };
+    Type: { data: { id: string } };
+  };
+};
+
+export type GraphQLSponsorType = {
+  id: string;
+  attributes: {
+    Name: string;
+    Order: number;
+    LogoSize: string;
+  };
+};
+
 export interface SponsorType {
-    Id: string
-    Name: string
-    Order: number
-    LogoSize: string
+  Id: string;
+  Name: string;
+  Order: number;
+  LogoSize: string;
 }
 
 export interface Sponsor {
-    Name: string
-    Logo: Image
-    Type: SponsorType
+  Name: string;
+  Logo: Image;
+  TypeID: string;
 }
 
 export interface Image {
-    Src: string
-    Width: string
-    Height: string
+  Url: string;
+  Width: number;
+  Height: number;
 }
