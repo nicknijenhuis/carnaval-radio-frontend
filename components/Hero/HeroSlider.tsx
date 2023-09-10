08024475d9b85d6cd82d2987d2a306b34fa296e5
ihtisham914 <shaami.khn321@gmail.com>
2023-09-11 00:55:44 +0500

    Fixed some bugs

--- components/Hero/HeroSlider.tsx
+++ components/Hero/HeroSlider.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { slides } from "../../public/ProjectData/slides";
