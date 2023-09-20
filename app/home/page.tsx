27dbcbdca05a6255ac9aeb414238d098cf625d6c
ihtisham914 <shaami.khn321@gmail.com>
2023-09-20 16:53:01 +0500

    fetched articles on server side for better perfomance

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,20 @@
-import { useEffect } from "react";
 import Hero from "@/components/Hero/Hero";
 import Sponsors from "@/components/Sponsors/Sponsors";
 import PostCard from "@/components/Post/PostCard";
+import { GET_ALL_ARTICLES } from "@/GlobalState/ApiCalls/graphql/article_queries";
+import { client } from "@/GlobalState/ApiCalls/api.config";
 
-export default function Home() {
+const page = async () => {
+  const { data } = await client.query({
+    query: GET_ALL_ARTICLES,
+  });
   return (
     <section className="flex-grow">
       <Hero />
       <Sponsors />
-      <PostCard />
+      {data && <PostCard posts={data.articles.data} />}
     </section>
   );
-}
+};
+
+export default page;
--- components/Post/PostCard.tsx
+++ components/Post/PostCard.tsx
@@ -1,59 +1,35 @@
-"use client";
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment } from "react";
 import { Post } from "../../types/articleTypes";
 import SectionTitle from "../constants/SectionTitle";
 import PostDetails from "./PostDetails";
 import news from "../../public/news.png";
-import { client } from "@/GlobalState/ApiCalls/api.config";
-import { GET_ALL_ARTICLES } from "@/GlobalState/ApiCalls/graphql/article_queries";
+
 import PostsSkeleten from "../LoadingSkeleten/PostsSkeleten";
 import Link from "next/link";
+interface props {
+  posts?: Post[];
+}
 
-const PostCard = () => {
-  const [posts, setPosts] = useState<Post[]>();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<any>();
-
-  const fetchPosts = async () => {
-    const { loading, error, data } = await client.query({
-      query: GET_ALL_ARTICLES,
-    });
-    setPosts(data.articles.data);
-    setLoading(loading);
-    setError(error);
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  }, [error]);
-
+const PostCard = ({ posts }: props) => {
   return (
     <div className="py-8 px-4 sm:px-4 md:px-8 lg:px-8 xl:px-8">
       <div className="flex justify-between items-center">
         <SectionTitle title="Nieuws" icon={news} />
       </div>
 
-      {!error && (
-        <div
-          className="flex justify-start flex-wrap
+      <div
+        className="flex justify-start flex-wrap
        gap-3 md:gap-6 m-auto pt-2 sm:pt-2 md:pt-6 lg:pt-10 xl:pt-10 w-full"
-        >
-          {!loading ? (
-            <>
-              {posts &&
-                posts.map((post: any, i: any) => (
-                  <Fragment key={"postFrag" + i}>
-                    {i < 3 && (
-                      <PostDetails key={"post" + i} post={post} i={i} />
-                    )}
-                  </Fragment>
-                ))}
-            </>
-          ) : (
-            <PostsSkeleten />
-          )}
-        </div>
-      )}
+      >
+        <>
+          {posts &&
+            posts.map((post: any, i: any) => (
+              <Fragment key={"postFrag" + i}>
+                {i < 3 && <PostDetails key={"post" + i} post={post} i={i} />}
+              </Fragment>
+            ))}
+        </>
+      </div>
 
       <div className="flex items-center justify-center pt-8">
         <Link
