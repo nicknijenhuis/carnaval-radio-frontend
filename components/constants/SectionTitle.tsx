f6540f93fe5afceb20ad381b5984dad8fea30981
ihtisham914 <shaami.khn321@gmail.com>
2023-10-25 15:03:31 +0500

    Improved responsive for Limburg24 news

--- components/constants/SectionTitle.tsx
+++ components/constants/SectionTitle.tsx
@@ -11,9 +11,28 @@ interface Props {
 const SectionTitle = ({ title, image, iconElement }: Props) => {
   return (
     <div className="flex space-x-4 items-center">
-      {image && <Image src={image} height={100} width={100} className="h-8 w-8" alt="" />}
+      {image && (
+        <Image
+          src={image}
+          height={100}
+          width={100}
+          className="h-8 w-8"
+          alt=""
+        />
+      )}
       {iconElement && iconElement}
-      <h2 className={`text-3xl font-semibold ${Indie.className}`}>{title}</h2>
+      {title == "Limburg Alaaf - Limburg24" && (
+        <h2
+          className={`block sm:hidden md:hidden lg:hidden xl:hidden text-3xl font-semibold ${Indie.className}`}
+        >
+          {title.split(" ")[3]}
+        </h2>
+      )}
+      <h2
+        className={`hidden sm:block md:block lg:block xl:block 2xl:block text-3xl font-semibold ${Indie.className}`}
+      >
+        {title}
+      </h2>
     </div>
   );
 };
