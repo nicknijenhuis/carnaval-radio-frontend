1955da998ce743e476555311a224c6c47f81e6ca
Nick Nijenhuis <nick@novana.nl>
2023-11-02 02:38:42 +0100

    Use before files rewrites

--- next.config.js
+++ next.config.js
@@ -6,24 +6,26 @@ const nextConfig = {
     domains: ["res.cloudinary.com", "ams1.reliastream.com"],
   },
   async rewrites() {
-    return [
-      {
-        source: "/verzoekjes",
-        destination: "/requests",
-      },
-      {
-        source: "/gedraaide-nummers",
-        destination: "/recentSongs",
-      },
-      {
-        source: '/nieuwsberichten',
-        destination: '/articles',
-      },
-      {
-        source: '/nieuwsberichten/:path*',
-        destination: '/articles/:path*',
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: "/verzoekjes",
+          destination: "/requests",
+        },
+        {
+          source: "/gedraaide-nummers",
+          destination: "/recentSongs",
+        },
+        {
+          source: '/nieuwsberichten',
+          destination: '/articles',
+        },
+        {
+          source: '/nieuwsberichten/:path*',
+          destination: '/articles/:path*',
+        },
+      ]
+    }
   },
   async redirects() {
     return [
