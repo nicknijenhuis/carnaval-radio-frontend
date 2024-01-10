06002c6e83203131f325c166b700c6275036e398
Nick <nick@novana.nl>
2024-01-10 22:22:45 +0100

    Add protective token to requests page

--- app/requests/page.tsx
+++ app/requests/page.tsx
@@ -8,7 +8,34 @@ export const metadata: Metadata = {
     "Doe nu je eigen verzoekje! De vastelaoves Radio | 24/7 Vastelaovend Muzieek",
 };
 
-const RequestsPage = () => {
+const RequestsPage = ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+  const token = searchParams["token"] ?? ""; 
+  const validToken = process.env.VALID_TOKEN;
+
+  if (token !== validToken) {
+    return (
+      <>
+        <div className="py-8 px-4 sm:px-4 md:px-8 lg:px-8 xl:px-8 bg-heroBackground">
+          <div className="p-8 rounded-3xl bg-white max-w-3xl">
+            <h2
+              className={`text-3xl text-primary mb-5 font-bold ${Indie.className}`}
+            >
+              Verzoekjes
+            </h2>
+            <p className="text-lg text-gray-700 mb-5">
+              Je hebt geen toegang tot deze pagina. Neem contact op met de
+              beheerder van deze website.
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <div className="py-8 px-4 sm:px-4 md:px-8 lg:px-8 xl:px-8 bg-heroBackground">
