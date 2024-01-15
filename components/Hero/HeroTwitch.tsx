f0b86851a202eec20d7ea133f811f1923626d0b8
Nick <nick@novana.nl>
2024-01-15 20:22:44 +0100

    better twitch integration

--- components/Hero/HeroTwitch.tsx
+++ components/Hero/HeroTwitch.tsx
@@ -3,7 +3,7 @@
 const HeroTwitch = () => {
     return (
       <>
-        <div style={{minHeight: '19rem'}} className="rounded-xl min-w-[30vw] min-h-16 shadow-lg md:ml-5 bg-gradient-to-r from-yellow-500 to-orange-500 pb-4 xl:pb-0 xl:pt-4">
+        <div style={{minHeight: '19rem'}} className="rounded-xl min-w-[30vw] min-h-16 shadow-lg bg-gradient-to-r from-yellow-500 to-orange-500">
           <iframe src="/twitch.html" className="w-full h-full" />
         </div>
      </>
