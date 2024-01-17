b5cd200d4123aec337356709eb5a96c1f6dbf346
Nick <nick@novana.nl>
2024-01-17 01:41:36 +0100

    new sliders

--- public/ProjectData/slides.js
+++ public/ProjectData/slides.js
@@ -1,7 +1,10 @@
 const cloudinaryUrl = "https://res.cloudinary.com/dwzn0q9wj/image/upload/w_1000/q_auto/f_auto/";
 export const slides = [
   {
-    url: cloudinaryUrl + "showcase/soohskjyjuompkwrowfa.jpg",
+    url: cloudinaryUrl + "showcase/soohskjyjuompkwrowfa.jpg", // limburgse avond image
+  },
+  {
+    url: cloudinaryUrl + "showcase/erpqa1pwgru9oezle5jd.jpg", // marjolein
   },
   {
     url: cloudinaryUrl + "showcase/czvwhrwbbwfuonsswzsp.jpg",
@@ -10,42 +13,39 @@ export const slides = [
     url: cloudinaryUrl + "showcase/y7fjasaarcfakdqioxtl.jpg",
   },
   {
+    url: cloudinaryUrl + "showcase/artists/ihcz2r90tmjspitocbtm.jpg",
+  },
+  {
     url: cloudinaryUrl + "showcase/icp4mv7b3eiqr1l2jkwh.jpg",
-  },
-  // {
-  //   url: cloudinaryUrl + "showcase/a0z6ln89efaq36phjofx.jpg", even geen petertje
-  // },
-  {
-    url: cloudinaryUrl + "showcase/m6mprkb3b3kh2ivcv4xt.jpg",
-  },
-  {
-    url: cloudinaryUrl + "showcase/txbbacxngk2pdyydu6st.jpg",
-  },
-  {
-    url: cloudinaryUrl + "showcase/cegv7njmfragfo8gzfw7.jpg",
-  },
-  {
-    url: cloudinaryUrl + "showcase/z1ardqrj21jskg1oxg37.jpg",
-  },
-  {
-    url: cloudinaryUrl + "showcase/artists/u60fhrbimyzvtzklxcsk.jpg",
-  },
+  },  
   {
     url: cloudinaryUrl + "showcase/artists/ixlawspt2rtrnnpjeklf.jpg",
   },
   {
-    url: cloudinaryUrl + "showcase/artists/nyez6yb7uv93yitjrkal.jpg",
+    url: cloudinaryUrl + "showcase/tb8msxyp80k8jbo6e7i0.jpg", // pruuf mer
+  },
+  {
+    url: cloudinaryUrl + "showcase/artists/gdddiuq7fcph19jmygbq.jpg", // erwin
   },
   {
     url: cloudinaryUrl + "showcase/artists/b4k26dnbjix4ldtybhog.jpg",
   },
   {
-    url: cloudinaryUrl + "showcase/artists/ihcz2r90tmjspitocbtm.jpg",
+    url: cloudinaryUrl + "showcase/hedb6vh4izk3akqxx0i5.jpg", // carnaval radio group image
+  },
+  {
+    url: cloudinaryUrl + "showcase/artists/nyez6yb7uv93yitjrkal.jpg",
+  },
+  {
+    url: cloudinaryUrl + "showcase/tfxjfedqtrunkcmim4v3.jpg", // 2023 - party image
+  },
+  {
+    url: cloudinaryUrl + "showcase/artists/u60fhrbimyzvtzklxcsk.jpg",
+  },
+  {
+    url: cloudinaryUrl + "showcase/v34fqyghopq7jizffbxb", // promilaasj
   },
   {
     url: cloudinaryUrl + "showcase/artists/qtvjqokkpndeeme7hmea.jpg",
-  },
-  {
-    url: cloudinaryUrl + "showcase/artists/opeppammdhycxaoufmpt.jpg",
-  },
+  }
 ];
