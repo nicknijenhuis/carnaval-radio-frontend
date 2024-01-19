98484328d1798ebe6221d709c558145ed06ad5b0
Nick <nick@novana.nl>
2024-01-19 07:30:27 +0100

    Sort events by date descending

--- GlobalState/ApiCalls/graphql/events_queries.ts
+++ GlobalState/ApiCalls/graphql/events_queries.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const GET_ALL_EVENTS = gql`
-    query{
-        events {
+    query {
+        events(sort: "Date:desc") {
             data {
                 id
                 attributes {
