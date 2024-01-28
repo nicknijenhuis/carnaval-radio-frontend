87f5e14b9d6f6bc9d0a085b05c1a7b1521a12651
Nick <nick@novana.nl>
2024-01-28 16:49:58 +0100

    Update promo slides

--- public/ProjectData/slides.js
+++ public/ProjectData/slides.js
@@ -1,10 +1,10 @@
 const cloudinaryUrl = "https://res.cloudinary.com/dwzn0q9wj/image/upload/w_1000/q_auto/f_auto/";
 export const slides = [
   {
-    url: cloudinaryUrl + "showcase/soohskjyjuompkwrowfa.jpg", // limburgse avond image
+    url: cloudinaryUrl + "showcase/tfxjfedqtrunkcmim4v3.jpg", // 2023 - party image
   },
   {
-    url: cloudinaryUrl + "showcase/tfxjfedqtrunkcmim4v3.jpg", // 2023 - party image
+    url: cloudinaryUrl + "showcase/01-2024/o9anmwlwpwtnfxjbd3sb.jpg", // 2024 - group image
   },
   {
     url: cloudinaryUrl + "showcase/erpqa1pwgru9oezle5jd.jpg", // marjolein
@@ -19,25 +19,13 @@ export const slides = [
     url: cloudinaryUrl + "showcase/tb8msxyp80k8jbo6e7i0.jpg", // pruuf mer
   },
   {
-    url: cloudinaryUrl + "showcase/artists/gdddiuq7fcph19jmygbq.jpg", // erwin
-  },
-  {
-    url: cloudinaryUrl + "showcase/artists/b4k26dnbjix4ldtybhog.jpg",
-  },
-  {
-    url: cloudinaryUrl + "showcase/hedb6vh4izk3akqxx0i5.jpg", // carnaval radio group image
+    url: cloudinaryUrl + "showcase/01-2024/tjcork7fx7g7oqemr7ks.jpg", // 2024 - group image
   },
   {
     url: cloudinaryUrl + "showcase/czvwhrwbbwfuonsswzsp.jpg",
   },
   {
-    url: cloudinaryUrl + "showcase/artists/u60fhrbimyzvtzklxcsk.jpg",
-  },
-  {
     url: cloudinaryUrl + "showcase/v34fqyghopq7jizffbxb", // promilaasj
-  },
-  {
-    url: cloudinaryUrl + "showcase/artists/nyez6yb7uv93yitjrkal.jpg",
   },
   {
     url: cloudinaryUrl + "showcase/icp4mv7b3eiqr1l2jkwh.jpg",
