ef6baa63298523595b02c69e56fb45dfa3aa371e
nicknijenhuis <n.nijenhuis@action.nl>
2025-01-04 13:11:15 +0100

    Fix events query

--- GlobalState/ApiCalls/graphql/events_queries.ts
+++ GlobalState/ApiCalls/graphql/events_queries.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const GET_ALL_EVENTS = gql`
     query {
-        events(sort: "Date:asc") {
+        events(sort: "Date:asc", pagination: { limit: 100 }) {
             data {
                 id
                 attributes {
